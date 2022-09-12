c7548eb97b11156a81fd99a4a655a640d155dc2a
nadjaekaterina <nadjaekaterina@web.de>
2022-09-12 10:47:36 +0000

    production tweaks

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*z#00hdd+)gt5cmp+yhd1$czxv%4ya@#ls0!9_2z5m4m0(h8en"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['spanishflashcards.pythonanywhere.com']
 
 
 # Application definition
